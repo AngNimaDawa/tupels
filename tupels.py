7afc68065888215324c9dc3f9e0ec897d8fefabf
AngNimaDawa <45613061+AngNimaDawa@users.noreply.github.com>
2018-12-14 08:36:28 +0545

    Create tupels.py

---
+++ tupels.py
@@ -0,0 +1,16 @@
+# # #tuples-()
+# # a = []
+# # b = []
+# # import sys 
+# # print(sys.getsizeof(a))
+# # print(sys.getsizeof(b))
+
+
+
+# a = [1,2,3]
+# t = [1,2,3]
+# result = t + tuples(a)
+# # print(result)
+# a = tuple(l)
+# special_add = t._add_(a)
+# print(special_add)
